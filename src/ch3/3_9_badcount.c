d5845934bc2a7d4adcebf2e7af7a0a5994ef72d0
hollis7 <herryhollis@163.com>
2025-07-29 22:01:35 +0800

    测试不正确的参数个数会有什么结果

--- src/ch3/3_9_badcount.c
+++ src/ch3/3_9_badcount.c
@@ -5,13 +5,12 @@
 
 int main(void)
 {
-    int f = 4;
-    int g = 5;
-    float h = 5.0f;
-    
-    printf("%d\n", f, g);
-    printf("%d %d\n", f);
-    printf("%d %f\n", h, g);
-
+    int n = 4;
+    int m = 5;
+    float f = 7.0f;
+    float g = 8.0f;
+    printf("%d\n", n, m);    /*参数太多　*/
+    printf("%d %d %d\n", n); /*参数太少*/
+    printf("%d %d\n", f, g); /*值的类型不匹配*/
     return 0;
 }

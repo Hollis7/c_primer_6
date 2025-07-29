2352e49675ade6812d75945a489e98d9df5e6cb2
hollis7 <herryhollis@163.com>
2025-07-29 21:23:12 +0800

    3-5

--- src/ch3/3_5_charcode.c
+++ src/ch3/3_5_charcode.c
@@ -2,10 +2,10 @@
 
 int main(void)
 {
-    char ch;
-    printf("Please enter a character.\n");
-    scanf("%c", &ch);
-    printf("The code for %c is %d.\n", ch, ch);
+	char ch;
+	printf("Please enter a character.\n");
+	scanf("%c", &ch);
+	printf("The code for %c is %d.\n", ch, ch);
 
 	return 0;
 }

c51b22b4ecb892dc698d489b0ec8d676f343790c
hollis7 <herryhollis@163.com>
2025-07-31 21:37:58 +0800

    hello

--- src/ch1/hello.c
+++ src/ch1/hello.c
@@ -1,13 +1,6 @@
 #include <string.h>
 #include <stdio.h>
-add(float a, float b)
-{
-    return a + b;
-}
 int main()
 {
-    float x = 1.0, y = 2.0;
-    auto result = add(x, y);
-    printf("%f\n", result);
     return 0;
 }
